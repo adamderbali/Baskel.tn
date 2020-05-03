25c45ce7afefb468f75c072b3eeb27da2b5c91e3
Khaled Naili <khaled.naili@esprit.tn>
2020-05-03 14:01:02 +0100

    CrudReparateur

---
+++ src/edu/baskel/test/Guitestmain.java
@@ -0,0 +1,49 @@
+package edu.baskel.test;
+
+
+/**
+ *
+ * @author ASUS
+ */
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Guitestmain extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../gui/reparateurGUI/ReparateurDetails.fxml"));
+        stage.initStyle(StageStyle.DECORATED);
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+   
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        launch(args);
+        
+        
+    }
+        
+}
