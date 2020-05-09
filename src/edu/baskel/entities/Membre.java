8b66f59bfc4739f8637bb7358436332ce7106b75
SkanderLazghab <64615966+SkanderLazghab@users.noreply.github.com>
2020-05-09 15:59:34 +0200

    Update_Membre_rec

--- src/edu/baskel/entities/Membre.java
+++ src/edu/baskel/entities/Membre.java
@@ -103,14 +103,14 @@ public class Membre {
         this.type_u = type_u;
     }
 //Constructeur skan
-    public Membre(int id_u, String nom_u, String prenom_u, int nbr_ban_u) {
+    public Membre(int id_u, String nom_u, String prenom_u, int nbr_rec_u) {
         this.id_u = id_u;
         this.nom_u = nom_u;
         this.prenom_u = prenom_u;
         this.nbr_rec_u = nbr_rec_u;
     }
     
-    public Membre(int id_u, int nbr_ban_u) {
+    public Membre(int id_u, int nbr_rec_u) {
         this.id_u = id_u;
         this.nbr_rec_u = nbr_rec_u;
     }
